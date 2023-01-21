eb918060c84998ecd14124eab93c3d892db3348f
dmitryduy <cdcvh14@gmail.com>
2023-01-21 17:03:23 +0300

    feat: add bottom menu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-import './App.css';
+import styles from './App.module.scss';
 import BottomMenu from './components/BottomMenu/BottomMenu';
 
 const App = () => {
-  return <BottomMenu/>;
+  return <div className={styles.container}>
+    <div className={styles.content}>
+
+    </div>
+    <BottomMenu/>
+  </div>;
 };
 export default App;
--- frontend/src/components/BottomMenu/BottomMenu.tsx
+++ frontend/src/components/BottomMenu/BottomMenu.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import cn from 'classnames';
+
+import {ReactComponent as StatisticSvg} from '../../assets/icons/statistic.svg';
+import {ReactComponent as MainSvg} from '../../assets/icons/main.svg';
+import {ReactComponent as FireSvg} from '../../assets/icons/fire.svg';
+import {ReactComponent as EatingSvg} from '../../assets/icons/eating.svg';
+import {ReactComponent as SettingsSvg} from '../../assets/icons/settings.svg';
 
 import styles from './BottomMenu.module.scss';
+
 
 const BottomMenu: React.FC = () => {
   return (
     <nav className={styles.container}>
-
+      <ul>
+        <li className={styles.item}><MainSvg/></li>
+        <li className={styles.item}><StatisticSvg/></li>
+        <li className={cn(styles.item, styles.mainIcon)}><FireSvg/></li>
+        <li className={styles.item}><EatingSvg/></li>
+        <li className={styles.item}><SettingsSvg/></li>
+      </ul>
     </nav>
   );
 };
